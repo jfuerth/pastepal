821806324a080074f39f1b1164edfb764fb41931
Jonathan Fuerth <jfuerth@gmail.com>
2016-06-12 19:55:06 -0400

    WIP: attempting to push new data with websockets

--- src/main/java/net/bluecow/pastepal/service/PastePalMain.java
+++ src/main/java/net/bluecow/pastepal/service/PastePalMain.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 @Configuration
 @ComponentScan
